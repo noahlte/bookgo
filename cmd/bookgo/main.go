931b54a3efd7b6ff513b75ae4c012c14f94f1d3b
noahlte <noahloute1995@gmail.com>
2026-02-18 16:11:39 +0100

    Init project

---
+++ cmd/bookgo/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/noahlte/bookgo/internal/command"
+)
+
+func main() {
+	command.Execute()
+}
---
+++ internal/command/command.go
@@ -0,0 +1,20 @@
+package command
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use: "bookgo",
+	Short: "Create a PDF Book from markdown files",
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
