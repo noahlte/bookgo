3f9aaed93489a2a62057df3a22223d4472f5ded8
noahlte <noahloute1995@gmail.com>
2026-02-24 18:36:15 +0100

    feat: section file scan

--- internal/service/build.go
+++ internal/service/build.go
@@ -13,66 +13,105 @@ import (
 )
 
 func BuildBook() error {
-	err := scanContent()
-	if err != nil {
-		return err
+	chapters, err := scanContent()
+	if err != nil { 
+		return err 
+	}
+
+	var userBook book.Book
+	err = userBook.UnmarshalBook()
+	if err != nil { 
+		return err 
+	}
+
+	userBook.Chapters = chapters
+
+	err = userBook.Save()
+	if err != nil { 
+		return err 
 	}
 
 	return nil
 }
 
 
-func scanContent() error {
+func scanContent() ([]book.Chapter, error) {
+	bookpath, err := os.Getwd()
+	if err != nil { 
+		return nil, err 
+	}
+
 	if _, err := os.Stat("content"); errors.Is(err, fs.ErrNotExist) {
-		return errors.New("no content directory found")
+		return nil, errors.New("no content directory found")
 	}
 
-	chapters, err := os.ReadDir("content")
+	folders, err := os.ReadDir("content")
 	if err != nil {
-		return errors.New("failed to read directory")
+		return nil, errors.New("failed to read directory")
 	}
 
-	if len(chapters) <= 0 {
-		return errors.New("content directory is empty")
+	if len(folders) <= 0 {
+		return nil, errors.New("content directory is empty")
 	}
 
-	for index, chapter := range chapters {
+	chapters := make([]book.Chapter, 0)
+
+	for index, chapter := range folders {
 		if chapter.IsDir() {
 			prefix := fmt.Sprintf("%d-chapter-", index + 1)
-			bookpath, err := os.Getwd()
-			if err != nil {
-				return err
+
+			chapterName, ok := strings.CutPrefix(chapter.Name(), prefix)
+			if !ok { 
+				return nil, errors.New("there has been an error while parsing file name") 
 			}
 
-			name, ok := strings.CutPrefix(chapter.Name(), prefix)
-			if !ok {
-				return errors.New("there has been an error while parsing file name")
+			chapterName = strings.ReplaceAll(chapterName, "-", " ")
+			chapterWords := strings.Fields(chapterName)
+			chapterName = util.Capitalize(chapterWords)
+
+			sections := make([]book.Section, 0)
+
+			files, err := os.ReadDir(path.Join(util.ContentDir, chapter.Name()))
+			if err != nil { 
+				return nil, err 
+			}
+			if len(files) == 0 { 
+				return nil, errors.New("a chapter can't be empty") 
 			}
 
-			name = strings.ReplaceAll(name, "-", " ")
-			capitalizeName := strings.Fields(name)
-			name = util.Capitalize(capitalizeName)
+			for _, section := range files {
+				if !strings.HasSuffix(section.Name(), ".md") { continue }
 
-			/*
-			TODO: Nested loop for Section
-				- Read chapter dir
-				- See if the dir is empty
-				- Check each file extension to see .md
-				- Analyze content --> to text
-				- Convert each file into Section struct
-				- Add Section truc to a Section array
-				- Add Section array to Chapter
-			*/
+				sectionName, ok := strings.CutSuffix(section.Name(), ".md")
+				if !ok { 
+					return nil, errors.New("can not cut suffix") 
+				}
+
+				sectionName = strings.ReplaceAll(sectionName, "-", " ")
+				sectionWords := strings.Fields(sectionName)
+				sectionName = util.Capitalize(sectionWords)
+
+				// TODO: Scan content
+
+				newSection := &book.Section{
+					Name: sectionName,
+					Path: path.Join(bookpath, util.ContentDir, chapter.Name(), section.Name()),
+				}
+
+				sections = append(sections, *newSection)
+			}
+
 
 			newChapter := &book.Chapter{
-				Name: name,
+				Name: chapterName,
 				Number: index + 1,
 				Path: path.Join(bookpath, util.ContentDir, chapter.Name()),
+				Sections: sections,
 			}
 
-			fmt.Println(newChapter)
+			chapters = append(chapters, *newChapter)
 		}
 	}
 
-	return nil
+	return chapters, nil
 }
